f4d49c4b893d1732b714b8283753fe2fdc370664
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-19 14:22:40 -0500

    Page now shows random hanzi

--- hello_world.dart
+++ hello_world.dart
@@ -1,16 +1,29 @@
 import 'dart:io';
 import 'dart:async';
+import 'dart:math';
+
+var rand = new Random();
+
+getRandomHanzi() {
+  const start = 0x4e00, end = 0x9fff;
+  const count = end - start + 1;
+
+  var n = rand.nextInt(count) + start;
+  return new String.fromCharCode(n);
+}
 
 Future main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8000);
-  print('Listening on localhost:${server.port}');
+  print('Listening on localhost: ${server.port}');
 
   await for (HttpRequest request in server) {
+    var hanzi = getRandomHanzi();
+
     request.response.headers.contentType =
-      new ContentType("text", "html", charset: "utf-8");
+      new ContentType('text', 'html', charset: 'utf-8');
 
     request.response
-      ..write('<h1>你好世界！</h1>')
+      ..write('<h1 style="font-size: 5rem">$hanzi</h1>')
       ..close();
   }
 }
