ca0c26ee531972fbc3655e9767a18a63310ad581
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-20 20:37:07 -0500

    Send an http request for Chicago business licenses

--- http_client.dart
+++ http_client.dart
@@ -1,16 +1,32 @@
-// import 'dart:io';
-// import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:uri/uri.dart';
 
-const url = 'http://ipecho.net/plain';
+const appToken = '';
+const baseUrl = 'https://data.cityofchicago.org/resource/xqx5-8hwx.json';
+
+String buildUrl() {
+  final startDate = new DateTime.now().subtract(new Duration(days: 90));
+  final startDateStr = new DateFormat("y-M-d").format(startDate) + "T00:00:00.000";
+  final query = """
+  application_type = 'ISSUE' AND
+  license_status = 'AAI' AND
+  license_start_date >= '$startDateStr' AND
+  within_circle(location, 41.972116, -87.689468, 1600)
+  """.replaceAll('\n', '');
+  final template = UriTemplate(baseUrl + r'{?app_token,where}');
+  // We need to replace here because UriTemplate is not able to accept param
+  // names that start with $.
+  var url = template.expand({"app_token": appToken, "where": query})
+    .replaceFirst('app_token=', r'$$app_token=')
+    .replaceFirst('where=', r'$where=');
+  return url;
+}
 
 Future main() async {
-  // Using the standard library:
-  // HttpClientRequest req = await HttpClient().getUrl(Uri.parse(url));
-  // HttpClientResponse res = await req.close();
-  // await res.transform(utf8.decoder).forEach(print);
-
+  var url = buildUrl();
+  print(url);
   String data = await http.read(url);
   print(data);
 }
