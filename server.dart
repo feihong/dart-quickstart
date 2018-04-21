d6b6d9e8f554fe41c9acd1abf4193c1e85981b46
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-20 22:35:39 -0500

    Get port from ini file instead of command line argument

--- server.dart
+++ server.dart
@@ -1,21 +1,25 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:math';
+import 'package:ini/ini.dart';
 
 var rand = new Random();
 
-getRandomHanzi() {
+String getRandomHanzi() {
   const start = 0x4e00, end = 0x9fff;
   const count = end - start + 1;
   var n = rand.nextInt(count) + start;
   return new String.fromCharCode(n);
 }
 
-Future main(List<String> arguments) async {
-  var port = 8000;
-  if (arguments.length > 0) {
-    port = int.parse(arguments[0]);
-  }
+Future<int> getPort() async {
+  var lines = await new File("server.ini").readAsLines();
+  var cfg = new Config.fromStrings(lines);
+  return int.parse(cfg.get('default', "port"));
+}
+
+Future main() async {
+  var port = await getPort();
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port);
   print('Listening on localhost: ${server.port}');
 
